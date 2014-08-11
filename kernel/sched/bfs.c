66b1541b447c8e11c696ac1a2596fb7053f68fdf
Derek <drod2169@gmail.com>
2014-08-11 08:08:34 -0400

    Revert "Try reinstating locks that affect cpu hotplug around sched affinity calls. -CK"
    This reverts commit d13bc1a013e98353adb22c0022f947625b1f661b.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -4408,13 +4408,11 @@ long sched_setaffinity(pid_t pid, const struct cpumask *in_mask)
 	struct task_struct *p;
 	int retval;
 
-	get_online_cpus();
 	rcu_read_lock();
 
 	p = find_process_by_pid(pid);
 	if (!p) {
 		rcu_read_unlock();
-		put_online_cpus();
 		return -ESRCH;
 	}
 
@@ -4471,7 +4469,6 @@ out_free_cpus_allowed:
 	free_cpumask_var(cpus_allowed);
 out_put_task:
 	put_task_struct(p);
-	put_online_cpus();
 	return retval;
 }
 
@@ -4517,7 +4514,6 @@ long sched_getaffinity(pid_t pid, cpumask_t *mask)
 	unsigned long flags;
 	int retval;
 
-	get_online_cpus();
 	rcu_read_lock();
 
 	retval = -ESRCH;
@@ -4535,7 +4531,6 @@ long sched_getaffinity(pid_t pid, cpumask_t *mask)
 
 out_unlock:
 	rcu_read_unlock();
-	put_online_cpus();
 
 	return retval;
 }
